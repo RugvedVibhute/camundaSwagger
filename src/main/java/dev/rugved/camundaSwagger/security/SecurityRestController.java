0fae1aee5c62d8886529b535281073fccfef64d6
RugvedVibhute <vibhuterugved@gmail.com>
2025-04-08 09:37:34 +0530

    deleted unnecessary files

--- src/main/java/dev/rugved/camundaSwagger/security/SecurityRestController.java
+++
@@ -1,8 +0,0 @@
-package au.com.optus.renaissanceCamunda.security;
-
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-
-@SecurityRequirement(name="bearerAuth")
-public interface SecurityRestController {
-
-}
